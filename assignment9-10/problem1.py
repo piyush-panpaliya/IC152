99d20500187485b9e20c82ffa979dc59c709da05
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2023-11-21 22:13:14 +0530

    done

--- assignment9-10/problem1.py
+++ assignment9-10/problem1.py
@@ -1,24 +1,54 @@
 import pandas as pd
-df = pd.read_csv("data/Cars93.csv")
-print(df)
+import sys
 
 
-def sortPrint(df):
+def partA():
+  df = pd.read_csv("data/Cars93.csv")
   df = df.sort_values('Model')
-  # df = df.reset_index(drop=True)
-  # select some column and add index+1 as another column
   df = df[['Model']].reset_index(drop=True)
   df.index += 1
   df.to_csv('problem1a.csv')
   print(df)
 
 
-sortPrint(df)
+partA()
 
-# Print all details of the costliest car of each of the ‘Types’.
-# Hints (print each, then choose the appropriate ones):
-# use price to find max
-idx = df.groupby('Type')['Price'].idxmax()
-costliest_cars = df.loc[idx]
-costliest_cars.to_csv('problem1b.csv')
-# df.groupby('Type').max().to_csv('problem1b.csv')
+
+def partB():
+  df = pd.read_csv('./data/Cars93.csv')
+  df = df.drop(columns=['Unnamed: 0'])
+  idx = df.groupby('Type')['Price'].idxmax()
+  costliest_cars = df.loc[idx]
+  costliest_cars.index += 1
+  costliest_cars.to_csv('problem1b.csv')
+
+
+partB()
+
+
+def partC(manuf):
+  df = pd.read_csv('./data/Cars93.csv')
+  if (manuf in df['Manufacturer'].values):
+    df = df[df['Manufacturer'] == manuf]
+    return df
+  return 'err'
+
+
+args = sys.argv[1:]
+if (len(args) != 1):
+  print('give valid input')
+  sys.exit(1)
+# only model name or everything?
+print(partC(args[0]))
+
+
+def partD():
+  df = pd.read_csv('./data/Cars93.csv')
+  df = df.groupby('Manufacturer').size()
+  df = df.reset_index(name='count')
+  df[''] = df.index + 1
+  df = df[['', 'Manufacturer', 'count']]
+  df.to_csv('problem1d.csv', index=False)
+
+
+partD()
